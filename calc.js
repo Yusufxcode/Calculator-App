1cb364296ea9e2e28c9216fdedc0b79c600e9097
Adebakin Yusuf <109008836+Yusufxcode@users.noreply.github.com>
2022-11-24 11:05:20 +0100

    Awesome Calculator

--- calc.js
+++ calc.js
@@ -1,48 +1,31 @@
-let num1 = document.querySelector('#num1');
-let num2 = document.querySelector('#num2');
-let symbol = document.querySelector('.symbol');
-let total = document.querySelector('.total');
-let add = document.querySelector('#add');
-let sub = document.querySelector('#sub');
-let mult = document.querySelector('#mult');
-let div = document.querySelector('#div');
-let reset = document.querySelector('#reset');
-let equalTo = document.querySelector('#equalTo');
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
 
-add.addEventListener('click', function(){
-    symbol.innerHTML = '+';
-})
-
-sub.addEventListener('click', function(){
-    symbol.innerHTML = '-';
-})
-
-mult.addEventListener('click', function(){
-    symbol.innerHTML = 'x';
-})
-
-div.addEventListener('click', function(){
-    symbol.innerHTML = '/';
-})
-
-equalTo.addEventListener('click', function(){
-    if(symbol.innerHTML === '+'){
-        let number = parseInt(num1.value) + parseInt(num2.value);
-        total.innerHTML = number;
-    }else if (symbol.innerHTML === '-'){
-        let number = parseInt(num1.value) - parseInt(num2.value);
-        total.innerHTML = number;
-    }else if (symbol.innerHTML === 'x'){
-        let number = parseInt(num1.value) * parseInt(num2.value);
-        total.innerHTML = number;
-    }else {
-        let number = parseInt(num1.value) / parseInt(num2.value);
-        total.innerHTML = number;
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    } else if (item.id == "backspace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != "" && item.id == "equal") {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Empty!";
+      setTimeout(() => (display.innerText = ""), 2000);
+    } else {
+      display.innerText += item.id;
     }
-})
+  };
+});
 
-reset.addEventListener('click', function(){
-    num1.value = '';
-    num2.value = '';
-    total.innerHTML = '0';
-})
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+};
+
